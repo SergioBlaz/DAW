ce57e1f704964dd57fc443e5a74e6ef27d9899a0
Sergio Blázquez <sergioblazquez.alu@iespacomolla.es>
2021-09-24 21:52:23 +0200

    sdg

--- DWEC/Tema2/Practicas/Workbench/h.js
+++ DWEC/Tema2/Practicas/Workbench/h.js
@@ -1,17 +1,36 @@
 "use strict";
 
-function Curso(PnomCurso, Panyo, Pdesc, Palumnado){
+function Curso(PnomCurso, Panyo, Pdesc){
     return{
         Nombre: PnomCurso,
         Anyo: Panyo,
         Descripcion: Pdesc,
-        Alumnado: Palumnado,
+        Alumnado:[],
+        //Revisar metodo matricular!!
+        matricular: function(estudiantes){
+            for(var prop in estudiantes){
+                for(var prop2 in estudiantes[prop]){
+                    this.Alumnado[this.Alumnado.length] = `${estudiantes[prop][prop2]}`;
+                }
+            }        
+        }
     }
 }
-const Curso1 = new Curso("2DAW",2021,"Segundo Curso de Daw",["Sergio","Manuel","Antonio","Pedro"]);
 
-function estudiante(nombre, apellido)
-
-Curso1.matricular = function (estudiantes) {
-    this.Alumnado = estudiantes.nombre + " " +estudiantes.apellidos;
+function Estudiante(pNombre, pApellidos){
+    return{
+        Nombre: pNombre,
+        Apellidos: pApellidos,
+    }
 }
+
+const listaEstudiantes = [
+    new Estudiante("Primer","Estudiante"),
+    new Estudiante("Segundo","Estudiante")
+];
+
+const Curso1 = new Curso("2DAW",2021,"Segundo Curso de Daw");
+
+Curso1.matricular(listaEstudiantes);
+
+console.log(Curso1.Alumnado);
