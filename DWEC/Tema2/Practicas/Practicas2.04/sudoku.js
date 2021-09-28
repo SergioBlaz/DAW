fb20fe8c6b6f8e597147b6d3c97a5a27e8793334
Sergio Blázquez <sergioblazquez.alu@iespacomolla.es>
2021-09-28 21:31:06 +0200

    sudoku

--- DWEC/Tema2/Practicas/Practicas2.04/sudoku.js
+++ DWEC/Tema2/Practicas/Practicas2.04/sudoku.js
@@ -1,6 +1,6 @@
 "use strict";
 
-sudokuCorrecto = []
+var sudokuCorrecto = []
 sudokuCorrecto[0] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 sudokuCorrecto[1] = [7, 8, 9, 1, 2, 3, 4, 5, 6];
 sudokuCorrecto[2] = [4, 5, 6, 7, 8, 9, 1, 2, 3];
@@ -11,27 +11,28 @@ sudokuCorrecto[6] = [2, 3, 1, 5, 6, 4, 8, 9, 7];
 sudokuCorrecto[7] = [8, 9, 7, 2, 3, 1, 5, 6, 4];
 sudokuCorrecto[8] = [5, 6, 4, 8, 9, 7, 2, 3, 1];
 
-sudokuIncorrecto = []
+var sudokuIncorrecto = []
 for(let fila of sudokuCorrecto) sudokuIncorrecto.push([...fila]);
 sudokuIncorrecto[0][0] = 2;
 
 function comprobarSudoku(sudoku) {
     var resultado ="Es correcto! Enhorabuena!!";
-    //var numero;
-    //var comprobante = [1,2,3,4,5,6,7,8,9];
-    //const bloque = 3;
+    var bloqueA=[];
+    var bloqueB=[];
+    var comprobante = [1,2,3,4,5,6,7,8,9];
 
-    for(let i=0; i<8; i++){
-    
-        for(let j=0; j<8; j++){
-            
-
-            
+    for(let i=0; i<9; i++){
+        for(let j=0; j<9; j++){
+            bloqueA[j]=sudoku[i][j];
+            bloqueB[j]=sudoku[j][i];
         }
-
+        bloqueA.sort();
+        bloqueB.sort();
+        
+        if(bloqueA[i]!=comprobante[i] || bloqueB[i]!=comprobante[i]){
+            resultado="Error!";
+        }
     }
     return resultado;
-
 }
-
 console.log(comprobarSudoku(sudokuCorrecto));
